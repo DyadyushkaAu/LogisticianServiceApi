1a4469231f10987db0fa432978e8520727cae146
DyadyushkaAu <z.kozhevnikoff102@yandex.ru>
2022-05-28 01:52:29 +0500

    updated

--- logistic_service/forms.py
+++ logistic_service/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Order
 from django.forms import TextInput
+import numbers
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
@@ -11,27 +12,72 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'email')
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Пароли не совпадают.')
-        return cd['password2']
+    def clean(self):
+        cd = super().clean()
+
+        if cd.get('password') != cd.get('password2'):
+            self.add_error('password', 'Пароли не совпадают')
+
 
 class OrdrsForm(forms.ModelForm):
+
+    phone_number = forms.CharField(label='Введите номер телефона',
+                                   widget=forms.TextInput(attrs={
+                                       'class': 'form-control',
+                                       'placeholder': 'Введите номер телефона'
+                                   }))
+
+    address = forms.CharField(label='Введите адрес',
+                                   widget=forms.TextInput(attrs={
+                                       'class': 'form-control',
+                                       'placeholder': 'Введите адрес'
+                                   }))
+
+    mass = forms.CharField(label='Введите массу',
+                                   widget=forms.TextInput(attrs={
+                                       'class': 'form-control',
+                                       'placeholder': 'Введите массу'
+                                   }))
 
     class Meta:
         model = Order
         fields = ['address', 'mass', 'phone_number', 'orderer', 'state']
-        widgets = {'address': TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите адрес'
-            }),
-            'phone_number': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Введите номер телефона'
-            }),
-            'mass': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Введите массу заказа'
-            })
+        widgets = {
+            # 'address': TextInput(attrs={
+            #     'class': 'form-control',
+            #     'placeholder': 'Введите адрес'
+            # }),
+            # 'phone_number': CharField(attrs={
+            #     'class': 'form-control',
+            #     'placeholder': 'Введите номер телефона'
+            # }),
+            # 'mass': TextInput(attrs={
+            #     'class': 'form-control',
+            #     'placeholder': 'Введите массу заказа'
+            # })
         }
+
+    def clean(self):
+        cd = super().clean()
+
+        print(cd.get('phone_number'), cd.get('address'), cd.get('mass'))
+
+        if cd.get('phone_number') is None:
+            self.add_error('phone_number', 'Введите номер телефона')
+        if type(get_value_type(cd, 'phone_number')) != int:
+            self.add_error('phone_number', 'Числовое значение пример: 89121545772')
+        if cd.get('address') is None:
+            self.add_error('address', 'Введите адресс')
+        if type(cd.get('address')) != str:
+            self.add_error('address', 'Текстовое значние')
+        if cd.get('mass') is None:
+            self.add_error('mass', 'Введите массу')
+        if type(get_value_type(cd, 'mass')) != int:
+            self.add_error('mass', 'Числовое значение')
+
+
+def get_value_type(cd, key):
+    try:
+        return int(cd.get(key))
+    except ValueError:
+        return str
--- logistic_service/views.py
+++ logistic_service/views.py
@@ -13,10 +13,22 @@ def signup(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return render(request, 'logistic_service/register_done.html', {'new_user': new_user})
+            return render(request, 'logistic_service/register_done.html', context={
+                'new_user': new_user,
+                'title': 'Регистрация завершина'
+            })
+        else:
+            user_form = UserRegistrationForm(request.POST)
+            return render(request, 'logistic_service/signup.html', context={
+                'user_form': user_form,
+                'title': 'Регистрация'
+            })
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'logistic_service/signup.html', {'user_form': user_form})
+        return render(request, 'logistic_service/signup.html', context={
+            'user_form': user_form,
+            'title': 'Регистрация'
+        })
 
 
 def Index(request):
@@ -32,11 +44,17 @@ def Catalogue(request):
 
 
 def MyOrders(request):
-    orders = Order.objects.all()
+    orders = Order.objects.filter(orderer=request.user)
     cities = City.objects.all()
     streets = Street.objects.all()
     districts = District.objects.all()
-    return render(request, 'logistic_service/orders.html', {'Orders': orders, 'cities': cities, 'streets': streets, 'districts': districts})
+    return render(request, 'logistic_service/orders.html', {
+        'orders': orders,
+        'cities': cities,
+        'streets': streets,
+        'districts': districts,
+        'user': request.user
+    })
 
 
 def CreateOrder(request):
@@ -44,9 +62,16 @@ def CreateOrder(request):
     if request.method == 'POST':
         form = OrdrsForm(request.POST)
         if form.is_valid():
+            form = form.save(commit=False)
+            form.orderer = request.user
+            form.state = 'Новый'
             form.save()
         else:
-            error = 'Форма была неверной'
+            print(form.errors)
+            form = OrdrsForm(request.POST)
+            return render(request,
+                          'logistic_service/createorder.html',
+                          context={'form': form, 'error': 'Форма неверна'})
 
     form = OrdrsForm()
     context = {
